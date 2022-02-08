46f4c0007e3e6dc33f338a5374e32258d699603e
ElwellGabriel <98167024+ElwellGabriel@users.noreply.github.com>
2022-02-08 13:38:15 -0500

    added.php page

---
+++ test_page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>test_page</title>
+    .test {
+      color: pink;
+    }
+  </head>
+  <body>
+
+    <div class="test">
+      <p> No </p>
+    </div>
+
+  </body>
+</html>
